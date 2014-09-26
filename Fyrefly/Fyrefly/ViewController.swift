7c5cd9913f7ed488ced6fdf1a62711e886b1c7a0
Eric Appel <ema77@cornell.edu>
2014-09-26 19:39:13 -0400

    initial

---
+++ Fyrefly/Fyrefly/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Fyrefly
+//
+//  Created by Eric Appel on 9/26/14.
+//  Copyright (c) 2014 Eric Appel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("FYREFLY")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
