35aa84af8c470a9ca4003447a48fe169b47f5f7e
Eric Appel <ema77@cornell.edu>
2014-09-26 19:59:27 -0400

    firebase integration

---
+++ Fyrefly/Fyrefly/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Fyrefly
+//
+//  Created by Eric Appel on 9/26/14.
+//  Copyright (c) 2014 Eric Appel. All rights reserved.
+//
+
+import Foundation
+
+let kFirebaseURL = "https://fyrefly.firebaseio.com"
--- Fyrefly/Fyrefly/ViewController.swift
+++ Fyrefly/Fyrefly/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         println("FYREFLY")
+        
+        //  Create reference to Firebase location
+        var myRootRef = Firebase(url: kFirebaseURL)
+        //  Write data to Firebase
+        myRootRef.setValue("FIREBASE DATA!!!")
+        
+        //  Read data and react to changes
+        myRootRef.observeEventType(.Value, withBlock: {
+            snapshot in
+            println("\(snapshot.name) -> \(snapshot.value)")
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
